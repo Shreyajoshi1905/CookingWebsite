1a9f87549e5f1d755341021da9834381574c40d9
Shreya Joshi <shreyajoshi1905@gmail.com>
2020-09-04 11:47:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Switch , Route , Redirect } from 'react-router-dom';
 //import "../node_modules/bootstrap/dist/js/bootstrap.bundle"; 
-import Home from "./Home";
-import About from "./About";
-import Service from "./Service";
-import Contact from "./Contact";
-import Navbars from "./Navbars";
 
 const App=() => {
+
+  const[name , setName] = useState("");
+  const[lastName , setLastName] = useState("")
+  const[fullName , setFullName] = useState("");
+  const[lastNameNew , setLastNameNew] = useState("");
+  
+  
+
+  const inputevent = (event) =>
+  {
+   console.log(event.target.value);
+   setName(event.target.value);
+
+  };
+
+  const inputeventTwo = (event) =>
+  {
+    console.log(event.target.value)
+   setLastName(event.target.value)
+
+  }
+
+  const onSubmits = (event) =>
+  {
+    setFullName(name);
+    setLastNameNew(lastName);
+
+    event.preventDefault();
+  }
   return (
+    <div>
+    <form  onSubmit = {onSubmits}>
     <div className="App">
-    <Navbars/>
-   <Switch>
-  <Route exact path = "/" component = {Home}/>
-  <Route exact path = "/about" component = {About}/>
-  <Route exact path = "/service" component = {Service}/>
-  <Route exact path = "/contact" component = {Contact}/>
-  <Redirect to ="/"/>
-        </Switch>
+    
+    <h1>login now</h1>
+   
+    <label>Hello {fullName} { lastNameNew} </label>
+    
+    <br/>
+
+    <input type = "text"
+    placeholder = "enter first name here"
+    onChange = {inputevent}
+    value = {name}
+     />
+
+     <br/>
+
+    <label  > last name</label>
+    <br/>
+    
+    <input type = "text"
+     placeholder = "enter your last name" 
+    onChange = {inputeventTwo}
+     value = {lastName} ></input><br/>
+
+     <lable> email</lable>
+     <br/>
+     <input 
+     type = "text"
+     //value = {email}
+     //onChange = {inputeventthree}
+    placeholder = "enter your email"></input>
+<br/>
+    <lable>password</lable>
+<br/>
+    <input type = "text"
+    placeholder ="write ypur pssword"
+    ></input>
+ 
+    <button>SUBMIT</button>
+  
     </div>
+    </form>
+    </div>
+    
+    
+
+
   );
 }
 
